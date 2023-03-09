71e7204c15dd27656d6379aba59b35db4f7e3bbd
ramazancivak <ramazancivak2018@gmail.com>
2023-03-09 13:46:29 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  base: '/xmlproduct',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/personaimport.github.io/'
+    : '/',
   transpileDependencies: true
 })
