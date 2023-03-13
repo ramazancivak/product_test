3dd3f97f7484e8971673f9a49ff09d3c6494e398
ramazancivak <ramazancivak2018@gmail.com>
2023-03-13 11:03:19 +0300

    netlify deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/personaimport.github.io/'
-    : '/',
     transpileDependencies: true
 })
